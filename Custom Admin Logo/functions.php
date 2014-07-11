0c611be8fa9dc844ba8caccbfe285d167ece2663
Baird Castleberry <baird@bairdcastleberry.com>
2014-07-11 12:41:00 -0500

    fixed link and cleaned

--- Custom Admin Logo/functions.php
+++ Custom Admin Logo/functions.php
@@ -1,25 +1,27 @@
-/* Custom Logo */
+/**** Custom Logo ****/
 
-function change_my_wp_login_image() {
+function custom_admin_logo() {
 echo "
 <style>
 body.login #login h1 a {
-background: url('".get_bloginfo('template_url')."/images/logo.jpg') 8px 0 no-repeat transparent;
+background: url('".get_bloginfo('template_url')."/images/VARIABLE_1.jpg') 8px 0 no-repeat transparent;
 height:42px;
 width:230px; }
 </style>
 ";
 }
-add_action("login_head", "change_my_wp_login_image");
+add_action("login_head", "custom_admin_logo");
 
-/* URL Title */
+/**** Custom Logo URL ****/
 
-function my_login_logo_url() {
-    return home_url();
+function custom_admin_logo_url() {
+    return 'VARIABLE_2';
 }
-add_filter( 'login_headerurl', 'my_login_logo_url' );
+add_filter( 'login_headerurl', 'custom_admin_logo_url' );
 
-function my_login_logo_url_title() {
-    return 'Whatever here';
+/**** Custom Logo URL Title ****/
+
+function custom_admin_logo_url_title() {
+    return 'VARIABLE_3';
 }
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+add_action("login_headertitle", "custom_admin_logo_url_title");
