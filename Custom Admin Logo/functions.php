963eb362ff830b57d67c1e4ec9d85cc0218d730f
Baird Castleberry <baird@bairdcastleberry.com>
2016-05-24 18:50:08 -0500

    meh

--- Custom Admin Logo/functions.php
+++ Custom Admin Logo/functions.php
@@ -4,7 +4,7 @@ function custom_admin_logo() {
 echo "
 <style>
 body.login #login h1 a {
-background: url('".get_bloginfo('template_url')."/images/logo.png') 8px 0 no-repeat transparent;
+background: url('".get_bloginfo('template_url')."/img/admin_logo.png') 8px 0 no-repeat transparent;
 height:61px;
 width:320px; }
 </style>
@@ -33,6 +33,6 @@ add_filter( 'login_headerurl', 'custom_admin_logo_url' );
 /**** Custom Logo URL Title ****/
 
 function custom_admin_logo_url_title() {
-    return 'VARIABLE_3';
+    return 'Need Support?';
 }
 add_action("login_headertitle", "custom_admin_logo_url_title");
