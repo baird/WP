62f4346f8b1a6783d2cd7742890b3942fa48d6fe
Baird Castleberry <baird@bairdcastleberry.com>
2020-10-29 09:34:56 -0500

    Update functions.php
    etc

--- functions.php
+++ functions.php
@@ -6,3 +6,12 @@ function remove_dash_comments() {
 	}  
 }
 add_action( 'admin_menu', 'remove_dash_comments' );
+
+/**** Remove post type menu item for all but admin dash ****/
+
+function remove_dash_menu_item() {
+	if ( !current_user_can('manage_options') ) {
+		remove_menu_page('edit.php?post_type=menu_item');
+	}  
+}
+add_action( 'admin_menu', 'remove_dash_menu_item' );
