173ac493a8df49b918f1495a17dc525347357352
Baird Castleberry <baird@bairdcastleberry.com>
2021-03-01 14:03:09 -0600

    Update functions.php
    debug menu keys

--- functions.php
+++ functions.php
@@ -15,3 +15,20 @@ function remove_dash_menu_item() {
 	}  
 }
 add_action( 'admin_menu', 'remove_dash_menu_item' );
+
+/**** debug menu keys ****/
+
+if (!function_exists('debug_menu_keys')):
+function debug_menu_keys() {
+if ( !is_admin())
+        return;
+    global $submenu, $menu, $pagenow;
+    if ( current_user_can('manage_options') ) {
+        if( $pagenow == 'index.php' ) {
+            echo '<pre>'; print_r( $menu ); echo '</pre>';
+            echo '<pre>'; print_r( $submenu ); echo '</pre>';
+        }
+    }
+}
+add_action( 'admin_notices', 'debug_menu_keys' );
+endif;
